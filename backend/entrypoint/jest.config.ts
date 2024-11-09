027780bafc3ef753d9cfcd089ba69059855a30ce
SleepyDude <ko_on@mail.ru>
2024-11-09 21:59:32 +0300

    feat: init nx and first nest service

---
+++ backend/entrypoint/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'entrypoint',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/backend/entrypoint',
+};
